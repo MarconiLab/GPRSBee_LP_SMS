7aded86f8f85a11eaa9043b15339ca8a8071f9d0
MarconiLab <rcarbaja@ictp.it>
2015-03-09 11:31:48 +0100

    Update GPRSBee_LP_SMS.ino

--- GPRSBee_LP_SMS.ino
+++ GPRSBee_LP_SMS.ino
@@ -14,6 +14,8 @@
   UartSBee Gnd <--> Gnd Stalker
   UartSBee Rx <--> DIAGPORT_TX Stalker
   UartSBee TX <--> DIAGPORT_RX Stalker
+  
+  Author: Rodrigo Carbajales MarconiLab
   */
 
 #include <avr/sleep.h>
